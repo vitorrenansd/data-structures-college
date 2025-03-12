1e94664d96eabc86cad6acc1869e44b692b13a92
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 15:26:33 -0300

    Usuário diz qual a operação
    Nesta nova versão o usuario consegue dizer qual calculo deseja fazer. Usei strings de simbolos +, -, * e /

--- src/org/vitorrenansd/Main.java
+++ src/org/vitorrenansd/Main.java
@@ -7,15 +7,29 @@ public class Main {
         Operacoes op1 = new Operacoes();
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Qual calculo irá fazer (+, -, *, /): ");
+        String calc = scan.nextLine();
+
         System.out.print("Valor de X: ");
         float x = scan.nextFloat();
 
         System.out.print("Valor de Y: ");
         float y = scan.nextFloat();
 
-        op1.adicao(x, y);
-        op1.subtracao(x, y);
-        op1.multiplicacao(x, y);
-        op1.divisao(x, y);
+
+        if (calc.equals("+")) {
+            op1.adicao(x, y);
+        }
+        else if (calc.equals("-")) {
+            op1.subtracao(x, y);
+        }
+        else if (calc.equals("*")) {
+            op1.multiplicacao(x, y);
+        }
+        else if (calc.equals("/")) {
+            op1.divisao(x, y);
+        } else {
+            System.out.println("Selecione uma opção válida");
+        } 
     }
 }
