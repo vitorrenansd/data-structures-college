06ae5f4e63429d451f08266210e88fafbb454c2f
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 15:47:31 -0300

    Create Operacoes.java

---
+++ src/org/vitorrenansd/classes/Operacoes.java
@@ -0,0 +1,19 @@
+package org.vitorrenansd.classes;
+
+public class Operacoes {
+    protected Operacoes() {
+    }
+
+    protected float adicao(float x, float y) {
+        return (x + y);
+    }
+    protected float subtracao(float x, float y) {
+        return (x - y);
+    }
+    protected float multiplicacao(float x, float y) {
+        return (x * y);
+    }
+    protected float divisao(float x, float y) {
+        return (x / y);
+    }
+}
