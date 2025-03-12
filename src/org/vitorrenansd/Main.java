b0b763040aab3e97150b44309db271a50969c819
VitorRenanSD <vitorrenansouzadias@gmail.com>
2025-03-12 15:47:33 -0300

    Update Main.java

--- src/org/vitorrenansd/Main.java
+++ src/org/vitorrenansd/Main.java
@@ -1,21 +1,11 @@
 package org.vitorrenansd;
 
-import java.util.Scanner;
+import org.vitorrenansd.classes.Util;
 
 public class Main {
     public static void main(String[] args) {
-        Operacoes op1 = new Operacoes();
-        Scanner scan = new Scanner(System.in);
+        Util ut = new Util();
 
-        System.out.print("Valor de X: ");
-        float x = scan.nextFloat();
-
-        System.out.print("Valor de Y: ");
-        float y = scan.nextFloat();
-
-        op1.adicao(x, y);
-        op1.subtracao(x, y);
-        op1.multiplicacao(x, y);
-        op1.divisao(x, y);
+        ut.userCalculo();
     }
 }
