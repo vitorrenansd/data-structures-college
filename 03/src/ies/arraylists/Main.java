fa5686e45d552245c70fc0f09cba8e9d8f86de5c
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-26 10:46:51 -0300

    Update Main.java

--- 03/src/ies/arraylists/Main.java
+++ 03/src/ies/arraylists/Main.java
@@ -1,24 +1,23 @@
 package ies.arraylists;
 
 import ies.arraylists.classes.*;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> myArr = new ArrayList<String>();
-        ArrayManipulation am = new ArrayManipulation();
+        ArrayManipulation list = new ArrayManipulation();
     
         // Tests
-        am.addNewElement(myArr, "TUF");
-        am.addNewElement(myArr, "Zotac");
-        am.addNewElement(myArr, "Aorus");
-        am.addNewElement(myArr, "ROG Strix");
-        am.addNewElement(myArr, "Asus");
-        am.addNewElement(myArr, "Gigabyte");
+        list.addNewElement("TUF");
+        list.addNewElement("Zotac");
+        list.addNewElement("Aorus");
+        list.addNewElement("ROG Strix");
+        list.addNewElement("Asus");
+        list.addNewElement("Gigabyte");
 
-        am.removeElement(myArr, 2);
+        list.removeElement(2);
 
-        System.out.println(am.getLength(myArr));
-        System.out.println(am.getElement(myArr, 2));
+        System.out.println(list.getLength());
+        
+        System.out.println(list.getElement(3));
     }
 }
