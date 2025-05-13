0e8075008be71f614d7c485d9ac1e6ee3c2978c9
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-13 15:59:14 -0300

    Testing insertionsort

--- 04/src/ies/main.py
+++ 04/src/ies/main.py
@@ -1,7 +1,8 @@
-import sorting.algorithm.BubbleSort as bs;
+import sorting.algorithm.BubbleSort as bubble;
+import sorting.algorithm.InsertionSort as insertion;
 
 data = [3, 4, 9, 2, 5, 1, 8]
 
 print(data)
-bs.bubbleSort(data)
+insertion.insertionSort(data)
 print(data)
