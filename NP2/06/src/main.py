9427df5bcd0fce2655aba7080d3ce18dec1165c5
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-14 11:10:53 -0300

    Tests in main.py

---
+++ NP2/06/src/main.py
@@ -0,0 +1,12 @@
+from classes.BinarySearchTree import BinarySearchTree
+
+tree = BinarySearchTree()
+tree.insert(20)
+tree.insert(10)
+tree.insert(30)
+tree.insert(25)
+
+# Buscando valores
+print(tree.search(10))  # True
+print(tree.search(15))  # False
+print(tree.search(25))  # True
