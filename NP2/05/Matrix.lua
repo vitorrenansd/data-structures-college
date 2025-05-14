b5220f6e212d9568f06b81e04e4befdf08747164
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-14 10:27:35 -0300

    Added Matrix class

--- NP2/05/Matrix.lua
+++ NP2/05/Matrix.lua
@@ -19,3 +19,27 @@ function Matrix:new(rows, cols)
     setmetatable(obj, Matrix)
     return obj
 end
+
+-- Method to fill the matrix with user input
+function Matrix:fill()
+    for i = 1, self.rows do
+        for j = 1, self.cols do
+            io.write(string.format("Enter value for position [%d][%d]: ", i, j))
+            local value = tonumber(io.read())
+            self.data[i][j] = value or 0
+        end
+    end
+end
+
+-- Method to count non-zero values in the matrix
+function Matrix:countNonZero()
+    local count = 0
+    for i = 1, self.rows do
+        for j = 1, self.cols do
+            if self.data[i][j] ~= 0 then
+                count = count + 1
+            end
+        end
+    end
+    return count
+end
