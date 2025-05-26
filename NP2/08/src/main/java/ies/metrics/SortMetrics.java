7ffe137bbd72edf758b113a0b96fc6275ad28b66
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-05-26 13:58:13 -0300

    Created class SortMetrics

--- NP2/08/src/main/java/ies/metrics/SortMetrics.java
+++ NP2/08/src/main/java/ies/metrics/SortMetrics.java
@@ -1,5 +1,23 @@
 package ies.metrics;
 
 public class SortMetrics {
-    
+    public int comparisons = 0;
+    public int swaps = 0;
+    public int recursiveCalls = 0;
+
+    // Reset metrics
+    public void reset() {
+        comparisons = 0;
+        swaps = 0;
+        recursiveCalls = 0;
+    }
+
+    // Print collected metrics
+    public void printMetrics(String algorithmName) {
+        System.out.println("|| " + algorithmName + " ||");
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+        System.out.println("Recursive Calls: " + recursiveCalls);
+        System.out.println();
+    }
 }
